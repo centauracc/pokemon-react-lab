547daf1ade132b16ce519600544388381c79056e
centauracc <centauracc@yahoo.com.sg>
2020-04-13 00:02:48 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,59 @@ import React from "react";
 import "./App.css";
 import pokemonData from "./pokemon/pokemon";
 
-const bulbasaur = pokemonData[0];
+function GetAllPokemons() {
+  let allPokemons = [];
 
-function PokemonCard({ pokemon }) {
-  const { id, name, type, base } = pokemon;
+  for (let i = 0; i < pokemonData.length; i++) {
+    allPokemons.push(PokemonCard(pokemonData[i]));
+  }
+
+  return allPokemons;
+}
+
+function PokemonCard({ id, name, type, base }) {
   return (
-    <div>
-      <div>{name.english}</div>
+    <div className="grid-item">
+      <div>
+        <img
+          src={process.env.PUBLIC_URL + "pokemonImage/" + id + ".png"}
+          className="photo"
+          alt={name.english}
+        />
+      </div>
+      <h2>{name.english}</h2>
+      <div>{PrintTypes(type)}</div>
+      <div>&nbsp;</div>
+      <div>{PrintAbilities(base)}</div>
     </div>
   );
 }
 
+function PrintTypes(type) {
+  return type.map((element) => {
+    return <span className={"type-" + element.toLowerCase()}>{element}</span>;
+  });
+}
+
+function PrintAbilities(base) {
+  let abilities = [];
+
+  Object.entries(base).map(([key, value]) => {
+    const ability = (
+      <div>
+        {key}: {value}
+      </div>
+    );
+    return abilities.push(ability);
+  });
+
+  return abilities;
+}
+
 function App() {
   return (
-    <div className="App">
-      <PokemonCard pokemon={bulbasaur} />
+    <div className="App grid-container">
+      <GetAllPokemons />
     </div>
   );
 }
